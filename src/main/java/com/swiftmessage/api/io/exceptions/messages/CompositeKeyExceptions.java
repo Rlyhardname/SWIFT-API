2e1a03e3e438370c6d73cf816be986af9917b453
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-05-12 16:09:19 +0300

    -add String representation messages for CompositeKeyExceptions / MessageIdentifierDuplicationException

---
+++ src/main/java/com/swiftmessage/api/io/exceptions/messages/CompositeKeyExceptions.java
@@ -0,0 +1,7 @@
+package com.swiftmessage.api.io.exceptions.messages;
+
+public class CompositeKeyExceptions {
+    public static String SENDER_REFERENCE_DUPLICATION = "Sender reference duplication for %s occurred, invalid message";
+    public static String TRANSACTION_REFERENCE_DUPLICATION = "Transaction duplication for %s occurred, invalid message";
+    public static String MAC_DUPLICATION = "MAC duplication for %s occurred, invalid message";
+}
