261cc50d02e71b619e7c4cd8de5e8f21d7279b49
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-05-13 16:01:46 +0300

    -remove messages because of duplication and move them in another package.

--- src/main/java/com/swiftmessage/api/exceptions/messages/ExceptionMessage.java
+++ src/main/java/com/swiftmessage/api/exceptions/messages/ExceptionMessage.java
@@ -1,9 +1,6 @@
 package com.swiftmessage.api.exceptions.messages;
 
 public final class ExceptionMessage {
-    public static final String SENDER_REFERENCE_DUPLICATION = "Sender reference already recorded in the current message!";
-    public static final String TRANSACTION_REFERENCE_DUPLICATION = "Transaction reference already recorded in the current message!";
-    public static final String MAC_REFERENCE_DUPLICATION = "MAC reference already recorded in the current message!";
     public static final String INVALID_ID = "Part of the identification is missing or corrupted!";
     public static final String MESSAGE_WITH_SIGNATURE_DOES_NOT_EXIST = "A message with such a SenderReference or Transaction reference does not exist!";
 }
